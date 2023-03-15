8af65478bd4988db238e952261d92cccd78b2673
Nikita <nilkanthnikita24@gmail.com>
2023-03-15 23:17:54 +0530

    NewBranchPrgm

---
+++ src/main/java/com/example/yatra/Visa.java
@@ -0,0 +1,17 @@
+package com.example.yatra;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+public class Visa {
+    @GetMapping("/visa")
+    public String getData(){
+        return  "G0 anywhere";
+    }
+}
